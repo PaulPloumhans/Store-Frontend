413c2ecf0459ce9f25e6b0b2b13fc86e479f63ab
PaulPloumhans <72436553+PaulPloumhans@users.noreply.github.com>
2021-10-12 16:45:48 +0200

    list of hard coded components displays

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from 'src/app/models/Product';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
-  styleUrls: ['./product-item.component.css']
+  styleUrls: ['./product-item.component.css'],
 })
 export class ProductItemComponent implements OnInit {
+  @Input() product: Product;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.product = new Product();
   }
 
+  ngOnInit(): void {}
 }
