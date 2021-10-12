54d6288bfab44bc831b77391192283b4502acee6
PaulPloumhans <72436553+PaulPloumhans@users.noreply.github.com>
2021-10-12 17:32:58 +0200

    added reading from JASON file using http module and observable

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/Product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -9,24 +10,9 @@ import { Product } from 'src/app/models/Product';
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
 
-  constructor() {}
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
-    this.products = [
-      {
-        id: 1,
-        name: 'Book',
-        price: 9.99,
-        url: 'https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-        description: 'You can read it!',
-      },
-      {
-        id: 2,
-        name: 'Headphones',
-        price: 249.99,
-        url: 'https://images.unsplash.com/photo-1583394838336-acd977736f90?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
-        description: 'Listen to stuff!',
-      },
-    ];
+    this.productService.getProducts().subscribe((res) => (this.products = res));
   }
 }
