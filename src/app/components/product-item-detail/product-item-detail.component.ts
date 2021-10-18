a466399832418201a0167e4c1a3bae040bf8558d
PaulPloumhans <72436553+PaulPloumhans@users.noreply.github.com>
2021-10-18 12:28:27 +0200

    page with details added

--- src/app/components/product-item-detail/product-item-detail.component.ts
+++ src/app/components/product-item-detail/product-item-detail.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/models/Product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-item-detail',
   templateUrl: './product-item-detail.component.html',
-  styleUrls: ['./product-item-detail.component.css']
+  styleUrls: ['./product-item-detail.component.css'],
 })
 export class ProductItemDetailComponent implements OnInit {
-
-  constructor() { }
+  product: Product = new Product();
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
 
   ngOnInit(): void {
+    this.getProduct();
   }
 
+  async getProduct(): Promise<void> {
+    // collect ID from route
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.product = await this.productService.getProduct(id);
+  }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../models/Product';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -10,5 +10,14 @@ export class ProductService {
   constructor(private http: HttpClient) {}
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>('assets/data.json');
+  }
+  // Made this async otherwise imposible to select just one aray entry - this would be different with an actual API call
+  async getProduct(id: Number): Promise<Product> {
+    return new Promise((resolve, reject) => {
+      this.http.get<Product[]>('assets/data.json').subscribe((x) => {
+        const product = x.find((h) => (h.id as number) === id) as Product;
+        resolve(product);
+      });
+    });
   }
 }
